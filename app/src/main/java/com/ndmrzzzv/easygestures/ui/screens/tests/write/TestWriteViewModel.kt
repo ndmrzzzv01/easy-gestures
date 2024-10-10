2313154bd1a0388330dd0f1f7d41bee749b01cd0
Nadia <ndmorozova01@gmail.com>
2024-10-10 17:54:15 +0200

    Fix the logic of 'write test'

--- app/src/main/java/com/ndmrzzzv/easygestures/ui/screens/tests/write/TestWriteViewModel.kt
+++ app/src/main/java/com/ndmrzzzv/easygestures/ui/screens/tests/write/TestWriteViewModel.kt
@@ -1,6 +1,7 @@
 package com.ndmrzzzv.easygestures.ui.screens.tests.write
 
 import androidx.lifecycle.ViewModel
+import com.ndmrzzzv.domain.network.data.Question
 import com.ndmrzzzv.easygestures.ui.screens.tests.data.TestResult
 import com.ndmrzzzv.easygestures.utils.StudyData
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,14 +12,18 @@ class TestWriteViewModel: ViewModel() {
     private val _userAnswers = MutableStateFlow<List<String>>(emptyList())
     val userAnswers: StateFlow<List<String>> = _userAnswers
 
+    private val _shuffledQuestions = MutableStateFlow<List<Question>>(emptyList())
+
     init {
-        val questionsSize = StudyData.lesson?.questions?.size ?: 0
+        val lesson = StudyData.lesson
+        val questionsSize = lesson?.questions?.size ?: 0
         _userAnswers.value = List(questionsSize) { "" }
+        _shuffledQuestions.value = lesson?.questions?.shuffled() ?: listOf()
     }
 
     fun getLesson() = StudyData.lesson
 
-    fun getQuestionsInLesson() = StudyData.lesson?.questions?.shuffled() ?: listOf()
+    fun getQuestionsInLesson() = _shuffledQuestions.value
 
     fun updateUserAnswer(pageOfQuestion: Int, answer: String) {
         _userAnswers.value = _userAnswers.value.toMutableList().also { it[pageOfQuestion] = answer }
